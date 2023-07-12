c7e081ca460ccbd31a7646b2fa0741f1c4131bfb
Héctor H. Louzao Pozueco <louzaoh@gmail.com>
2023-07-12 10:18:00 +0200

    rename ejercicio6 files + add ejercicio2

---
+++ ejercicio6/ejercicio2.py
@@ -0,0 +1,21 @@
+class Alumno():
+    def __init__(self, nombre, nota):
+        self.nombre = nombre
+        self.nota = nota
+
+    def mostrar(self):
+        print(f"Nombre, {self.nombre}")
+        print("Nota: ", self.nota)
+
+    def valorar(self):
+        if self.nota < 5:
+            print(f"{self.nombre} ha suspendido")
+        else:
+            print(f"{self.nombre} ha aprobado")
+
+alumno1 = Alumno("Alumno 1", 4)
+alumno1.mostrar()
+alumno1.valorar()
+alumno2 = Alumno("Alumno 2", 7)
+alumno2.mostrar()
+alumno2.valorar()
